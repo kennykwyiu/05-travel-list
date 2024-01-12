1088cc6e2a358418ccefbe3dfc4adc2cbc20ea5d
kenny <kenny.kwyiu@gmail.com>
2024-01-13 00:00:04 +0800

    85-calculating stats

--- src/App.js
+++ src/App.js
@@ -36,7 +36,7 @@ export default function App() {
         onDeleteItem={handleDeleteItem}
         onToggleItem={handleToggleItem}
       />
-      <Stats />
+      <Stats items={items} />
     </div>
   );
 }
@@ -124,10 +124,25 @@ function Item({ item, onDeleteItem, onToggleItem }) {
   );
 }
 
-function Stats() {
+function Stats({ items }) {
+  if (!items.length)
+    return (
+      <p className="stats">
+        <em>Start adding some items to your packing list 🤩</em>
+      </p>
+    );
+
+  const numItems = items.length;
+  const numPacked = items.filter((item) => item.packed).length;
+  const percentage = Math.round((numPacked / numItems) * 100);
+
   return (
     <footer className="stats">
-      <em>🧰 You have X items on tour list, and you already packed X (X%)</em>
+      <em>
+        {percentage === 100
+          ? "You got everything! Ready to go👻"
+          : `🧰 You have ${numItems} items on tour list, and you already packed ${numPacked} (${percentage}%)`}
+      </em>
     </footer>
   );
 }
